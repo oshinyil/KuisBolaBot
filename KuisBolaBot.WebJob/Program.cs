1234ab3b15b5bef491260880f9d464a97401a921
Tane Yoroshi <taneyoroshi@gmail.com>
2017-01-15 10:29:38 +0800

    Added exception handling in the main loop.

--- KuisBolaBot.WebJob/Program.cs
+++ KuisBolaBot.WebJob/Program.cs
@@ -50,60 +50,67 @@ namespace KuisBolaBot.WebJob
             int offset = 0;
             while (!stopMe)
             {
-                var updates = bot.GetUpdatesAsync(offset).Result;
-                if (updates != null)
+                try
                 {
-                    foreach (var update in updates.Where(u => !updateIds.Contains(u.Id)))
+                    var updates = bot.GetUpdatesAsync(offset).Result;
+                    if (updates != null)
                     {
-                        offset = update.Id + 1;
-                        if (update.Message == null)
+                        foreach (var update in updates.Where(u => !updateIds.Contains(u.Id)))
                         {
-                            continue;
-                        }
-                        var from = update.Message.From;
-                        var text = update.Message.Text;
-                        Console.WriteLine(
-                            "Msg from {0} {1} ({2}) at {4}: {3}",
-                            from.FirstName,
-                            from.LastName,
-                            from.Username,
-                            text,
-                            update.Message.Date);
+                            offset = update.Id + 1;
+                            if (update.Message == null)
+                            {
+                                continue;
+                            }
+                            var from = update.Message.From;
+                            var text = update.Message.Text;
+                            Console.WriteLine(
+                                "Msg from {0} {1} ({2}) at {4}: {3}",
+                                from.FirstName,
+                                from.LastName,
+                                from.Username,
+                                text,
+                                update.Message.Date);
 
-                        if (string.IsNullOrEmpty(text))
-                        {
-                            continue;
-                        }
+                            if (string.IsNullOrEmpty(text))
+                            {
+                                continue;
+                            }
 
-                        if (text == "/start")
-                        {
-                            gameManager.Start(update.Message.Chat.Id, update.Message.From.Username);
-                        }
-                        else if (text == "/next")
-                        {
-                            gameManager.SendQuestion(update.Message.Chat.Id);
-                        }
-                        else if (text == "/join")
-                        {
-                            gameManager.Join(update.Message.Chat.Id, update.Message.From.Username);
-                        }
-                        else if (text == "/end")
-                        {
-                            gameManager.End(update.Message.Chat.Id, update.Message.From.Username);
-                        }
-                        else if (text == "/table")
-                        {
-                            gameManager.ShowTable(update.Message.Chat.Id, update.Message.From.Username);
-                        }
-                        else if (text == "/help")
-                        {
-                            gameManager.ShowHelp(update.Message.Chat.Id);
-                        }
-                        else
-                        {
-                            gameManager.Answer(update);
+                            if (text == "/start")
+                            {
+                                gameManager.Start(update.Message.Chat.Id, update.Message.From.Username);
+                            }
+                            else if (text == "/next")
+                            {
+                                gameManager.SendQuestion(update.Message.Chat.Id);
+                            }
+                            else if (text == "/join")
+                            {
+                                gameManager.Join(update.Message.Chat.Id, update.Message.From.Username);
+                            }
+                            else if (text == "/end")
+                            {
+                                gameManager.End(update.Message.Chat.Id, update.Message.From.Username);
+                            }
+                            else if (text == "/table")
+                            {
+                                gameManager.ShowTable(update.Message.Chat.Id, update.Message.From.Username);
+                            }
+                            else if (text == "/help")
+                            {
+                                gameManager.ShowHelp(update.Message.Chat.Id);
+                            }
+                            else
+                            {
+                                gameManager.Answer(update);
+                            }
                         }
                     }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception: {0}", ex.ToString());
                 }
             }
         }
